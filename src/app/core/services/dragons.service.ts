daf137c10306f8090f278e4057ebd8be94ad1859
Victor <victor.gvargas@outlook.com>
2022-05-20 11:12:22 -0300

    Created dragon service

--- src/app/core/services/dragons.service.ts
+++ src/app/core/services/dragons.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Dragon } from 'src/app/shared/models/dragon.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragonsService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getDragons(): Observable<Dragon[]> {
+    return this._http.get(`${environment.baseURL}`) as Observable<Dragon[]>;
+  }
+
+  getDragon(id: number): Observable<Dragon> {
+    return this._http.get(`${environment.baseURL}/${id}`) as Observable<Dragon>;
+  }
+
+  createDragon(dragon: Dragon): Observable<Dragon> {
+    return this._http.post(`${environment.baseURL}`, dragon) as Observable<Dragon>;
+  }
+
+  editDragon(dragon: Dragon, id: number) {
+    return this._http.put(`${environment.baseURL}/${id}`, dragon) as Observable<Dragon>;
+  }
+
+  deleteDragon(id: number) {
+    return this._http.delete(`${environment.baseURL}/${id}`) as Observable<Dragon>;
+  }
 }
---
+++ src/app/shared/models/dragon.model.ts
@@ -0,0 +1,7 @@
+export interface Dragon {
+    name: string;
+    createdAt: string | Date;
+    type: string;
+    histories: string[];
+    id: number;
+}
